0e6a5b84f603ced96c67d8b6b945737dcdda238f
CodeSandbox <support@codesandbox.io>
2025-07-31 05:27:16 +0000

    Sandbox snapshot commit

---
+++ src/components/About.tsx
@@ -0,0 +1,8 @@
+<a
+  href="/RB_Yogeshwar_Resume.pdf"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="mt-4 inline-block bg-white text-indigo-600 px-6 py-2 rounded-full font-semibold hover:bg-gray-200"
+>
+  View Resume
+</a>;
